10b7d41aeac28ecf7a46f4cf24216a775e21bf3c
riyadzaigirdar <riyadzaigir280@gmail.com>
2020-08-22 15:56:17 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,13 @@
+let allRead = true;
+
+let notifications = [
+  { message: "Lorem", read: true },
+  { message: "Ipsum", read: true },
+  { message: "Dolor", read: true },
+  { message: "Sit", read: false },
+  { message: "Amet", read: true },
+];
+
+allRead = notifications.every(({ read }) => read);
+
+console.log(allRead);
